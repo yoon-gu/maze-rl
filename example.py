046c4e268ee2d046f165cc1f92bd710e054c6173
Jacob Hwang <yz0624@gmail.com>
2021-05-14 22:48:02 +0900

    for -> while

--- example.py
+++ example.py
@@ -1,5 +1,4 @@
 from maze import MazeEnv, ACTIONS
-import numpy as np
 
 obstacle_positions = [(2, 2), (5, 3), (1, 3)]
 
@@ -9,8 +8,9 @@ state = env.reset()
 num_episode = 20
 done = False
 print(0, state, 0.0)
-for e in range(1, 1 + num_episode):
-    if not done:
-        action = env.action_space.sample()
-        new_state, reward, done, _ = env.step(action)
-        print(e, new_state, reward, ACTIONS[action])
+e = 1
+while not done:
+    action = env.action_space.sample()
+    new_state, reward, done, _ = env.step(action)
+    print(e, new_state, reward, ACTIONS[action])
+    e += 1
