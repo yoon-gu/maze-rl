e4c62fe2806ec84864c6d633a63f6011efb4ee52
Jacob Hwang <yz0624@gmail.com>
2021-05-20 13:41:43 +0900

    Update vis.py

--- vis.py
+++ vis.py
@@ -1,21 +1,34 @@
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
-from maze import Maze
+from maze import Maze, Obstacle, People
 
 def agent_portrayal(agent):
-    portrayal = {"Shape": "circle",
-                "Color": "red",
-                "Filled": "true",
-                "Layer": 0,
-                "Label": str(agent.reward_sum),
-                "r": 0.5}
+    if type(agent) is People:
+        portrayal = {"Shape": "circle",
+                    "Color": "red",
+                    "Filled": "true",
+                    "Layer": 1,
+                    "Label": str(agent.reward_sum),
+                    "r": 0.5}
+    elif type(agent) is Obstacle:
+        portrayal = {"Shape": "rect",
+                    "Color": "black",
+                    "Filled": "true",
+                    "Layer": 0,
+                    "w": 0.9,
+                    "h": 0.9}
     return portrayal
 
-grid = CanvasGrid(agent_portrayal, 4, 4, 500, 500)
+grid = CanvasGrid(agent_portrayal, 6, 6, 500, 500)
+
+obstacle_positions = [(2, 2), (5, 3), (1, 3), (3,5)]
 
 server = ModularServer( Maze,
                         [grid],
                         "Money Model",
-                        {"width":4, "height":1})
+                        {"width":6, "height":6,
+                        "start": (0, 0),
+                        "obstacle_positions": obstacle_positions
+                        })
 server.port = 8521 # The default
 server.launch()
